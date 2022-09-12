c95211a8e2b8f20b93faf3ecd87a779f8204d1af
Lauren Alvarez <laurenalvarez@Laurens-MacBook-Pro.local>
2022-09-12 15:14:04 -0400

    pretty medians of lime rankings for clone proof

--- metrics.py
+++ metrics.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 from sklearn.metrics import confusion_matrix,classification_report
-from JMeasure import measure_final_score,calculate_recall,calculate_precision,calculate_accuracy
+from Measure import measure_final_score,calculate_recall,calculate_precision,calculate_accuracy
 from cols import Table, Col, Sym, Num
 
 
